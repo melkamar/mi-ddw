cd9e70e52abdb11915b0e32d301c1533c06ac6a4
Martin Melka <melka@avast.com>
2017-02-27 12:28:12 +0100

    Lab done.

--- labs/1/crawler.py
+++ labs/1/crawler.py
@@ -1,26 +1,43 @@
-import requests
-from bs4 import BeautifulSoup
+import scrapy
 
 
-def crawler(seed):
-    frontier = [seed]
-    crawled = []
-    while frontier:
-        page = frontier.pop()
-        try:
-            print('Crawled:' + page)
-            source = requests.get(page).text
-            soup = BeautifulSoup(source, "html5lib")
-            links = soup.findAll('a', href=True)
+class CitySpider(scrapy.Spider):
+    name = 'melkaspider'
+    start_urls = ['http://127.0.0.1:8000']
 
-            if page not in crawled:
-                for link in links:
-                    frontier.append(link['href'])
-                crawled.append(page)
+    custom_settings = {
+        'USER_AGENT': 'DDW',
+        'DOWNLOAD_DELAY': 1.5,
+        'ROBOTSTXT_OBEY': True
+    }
 
-        except Exception as e:
-            print(e)
-    return crawled
+    def parse_person(self, response):
+        print('Parsing person.')
+        name = response.css('span.name::text').extract()
+        phone = response.css('span.phone::text').extract()
+        gender = response.css('span.gender::text').extract()
+        age = response.css('span.age::text').extract()
 
+        yield {
+            'name': name,
+            'phone': phone,
+            'gender': gender,
+            'age': age,
+        }
 
-crawler('https://fit.cvut.cz')
+    def parse_cities(self, response):
+        print("Parsing city.")
+        persons = response.css('ul.persons a')
+        persons_urls = persons.css('::attr(href)').extract()
+
+        for person_url in persons_urls:
+            yield scrapy.Request(response.urljoin(person_url), callback=self.parse_person)
+
+    def parse(self, response):
+        cities = response.css('ul.cities a')
+        print(cities.css('::text').extract())
+
+        cities_tocrawl = cities.css('::attr(href)').extract()
+
+        for city_url in cities_tocrawl:
+            yield scrapy.Request(response.urljoin(city_url), callback=self.parse_cities)
