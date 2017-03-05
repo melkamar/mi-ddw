d93fa4284f572eeca0646f2caa5b46cd844daf95
Martin Melka <melka@avast.com>
2017-03-05 17:57:01 +0100

    Add pagination browsing

--- homeworks/hw1/hw1/spiders/crawler.py
+++ homeworks/hw1/hw1/spiders/crawler.py
@@ -5,14 +5,21 @@ class IhnedSpider(scrapy.Spider):
     name = 'ihned'
     start_urls = ['http://byznys.ihned.cz/dalsi-clanky/']
 
+    scrape_pages_limit = 5
+
+    def __init__(self):
+        super().__init__()
+        self.scraped_pages = 0
+        self.scraped_articles = 0
+
     def parse_author(self, response):
         name = response.css('div.author-info a::text').extract_first()
         print("Parsing author: {}".format(name))
 
         # TODO figure out how to output separate items in json
-        yield {
-            'author_name': name
-        }
+        # yield {
+        #     'author_name': name
+        # }
 
     def parse_article(self, response):
         title = response.css('h1::text').extract_first()
@@ -49,9 +56,21 @@ class IhnedSpider(scrapy.Spider):
         }
 
     def parse(self, response):
+        if self.scraped_pages + 1 > self.scrape_pages_limit:
+            print("Already scraped {} pages. {} is limit, so returning.".format(self.scraped_pages, self.scrape_pages_limit))
+            return
+        else:
+            self.scraped_pages += 1
+
         print("Parsing article list page")
         articles_urls = response.css('div.article div.article-content a.article-more ::attr(href)').extract()
 
         for article_url in articles_urls:
             print(article_url)
             yield scrapy.Request(response.urljoin(article_url), callback=self.parse_article)
+
+        nextPage = scrapy.selector.HtmlXPathSelector(response).select(
+            "//div[@class='content-column left']/table/tr/td[@class='norm']/a[contains(text(),'Další')]/@href").extract_first()
+
+        print("Next page: {}".format(nextPage))
+        yield scrapy.Request(response.urljoin(nextPage), callback=self.parse)
