3e6946b22ffebfacf33914122da7d42511cca60f
Martin Melka <melka@avast.com>
2017-03-26 18:55:44 +0200

    HW2: Recall and precision metrics implemented. Missing F-measure

--- homeworks/hw2/indexing.py
+++ homeworks/hw2/indexing.py
@@ -1,32 +1,37 @@
-# import
-from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from collections import namedtuple
 import numpy as np
 import scipy.sparse
-from pprint import pprint
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
 
 data = []
+RELEVANT_DOCUMENT_COUNT = 150
 
-RELEVANT_DOCUMENT_COUNT = 10
+Weighting_Result = namedtuple("Weighting_Result", ["euclidean", "cosine"])
+
 
 def get_data():
     """ Return list of strings, each string is one document. """
-    return data if data else ['this is a sample string',
-                              'second string is like the first',
-                              "the the the xoxoxo"]
-    # return data if data else [open("./data/d/" + str(d + 1) + ".txt").read() for d in range(1400)]
+    # return data if data else ['this is a sample string',
+    #                           'second string is like the first',
+    #                           "the the the xoxoxo"]
+    return data if data else [open("./data/d/" + str(d + 1) + ".txt").read() for d in range(1400)]
 
 
 def get_queries():
     """ Return list of strings, each string is a query. """
-    return ['the string is a']
-    # return [open("./data/q/" + str(q) + ".txt").read() for q in [1]]
+    # return ['the string is a']
+    return [open("./data/q/" + str(q) + ".txt").read() for q in range(1, 226)]
 
 
 def get_relevant_docs(query_id):
     """ Return list of numbers denoting documents relevant to the given query. """
-    with open("./r/{}".format(query_id)) as f:
-        return f.readlines()
+    res = []
+    with open("./data/r/{}.txt".format(query_id)) as f:
+        for line in f.readlines():
+            res.append(int(line))
+
+    return res
 
 
 def calculate_distances_euclidean(query_vector, data_vectors):
@@ -84,10 +89,12 @@ def process_query_binary(query):
     # print("Query vector: {}".format(query_vector.toarray()))
     # print("Data vectors: {}".format(data_vectors.toarray()))
 
-    euclid_distances = calculate_distances_euclidean(query_vector, data_vectors)[:RELEVANT_DOCUMENT_COUNT]
-    cosine_similarities = calculate_cosine_similarity(query_vector, data_vectors)[:RELEVANT_DOCUMENT_COUNT]
+    euclid_distances = calculate_distances_euclidean(query_vector, data_vectors)
+    cosine_similarities = calculate_cosine_similarity(query_vector, data_vectors)
     print("Binary Euclid: {}".format(euclid_distances))
     print("Binary Cosine: {}".format(cosine_similarities))
+
+    return Weighting_Result(euclidean=euclid_distances, cosine=cosine_similarities)
 
 
 def process_query_term_frequency(query):
@@ -111,10 +118,12 @@ def process_query_term_frequency(query):
     # print("Query vector: {}".format(query_vector.toarray()))
     # print("Data vectors: {}".format(data_vectors.toarray()))
 
-    euclid_distances = calculate_distances_euclidean(query_vector, data_vectors)[:RELEVANT_DOCUMENT_COUNT]
-    cosine_similarities = calculate_cosine_similarity(query_vector, data_vectors)[:RELEVANT_DOCUMENT_COUNT]
+    euclid_distances = calculate_distances_euclidean(query_vector, data_vectors)
+    cosine_similarities = calculate_cosine_similarity(query_vector, data_vectors)
     print("TF Euclid: {}".format(euclid_distances))
     print("TF Cosine: {}".format(cosine_similarities))
+
+    return Weighting_Result(euclidean=euclid_distances, cosine=cosine_similarities)
 
 
 def process_query_tfidf(query):
@@ -132,18 +141,204 @@ def process_query_tfidf(query):
     # print("Query vector: {}".format(query_vector.toarray()))
     # print("Data vectors: {}".format(data_vectors.toarray()))
 
-    euclid_distances = calculate_distances_euclidean(query_vector, data_vectors)[:RELEVANT_DOCUMENT_COUNT]
-    cosine_similarities = calculate_cosine_similarity(query_vector, data_vectors)[:RELEVANT_DOCUMENT_COUNT]
+    euclid_distances = calculate_distances_euclidean(query_vector, data_vectors)
+    cosine_similarities = calculate_cosine_similarity(query_vector, data_vectors)
     print("TF-IDF Euclid: {}".format(euclid_distances))
     print("TF-IDF Cosine: {}".format(cosine_similarities))
 
+    return Weighting_Result(euclidean=euclid_distances, cosine=cosine_similarities)
+
+
+def calculate_precision(retrieved_documents, relevant_documents):
+    relevant_count = 0
+    for doc in retrieved_documents:
+        if doc in relevant_documents:
+            relevant_count += 1
+
+    return relevant_count / len(retrieved_documents)
+
+
+def calculate_recall(retrieved_documents, relevant_documents):
+    relevant_count = 0
+    for doc in retrieved_documents:
+        if doc in relevant_documents:
+            relevant_count += 1
+
+    return relevant_count / len(relevant_documents)
+
+
+Query_Results = namedtuple("Query_Results", ["binary_precision",
+                                             "binary_recall",
+                                             "tf_precision",
+                                             "tf_recall",
+                                             "tfidf_precision",
+                                             "tfidf_recall",
+                                             ])
+
 
 def process_queries():
-    queries = get_queries()
-    for query in queries:
-        process_query_binary(query)
-        process_query_term_frequency(query)
-        process_query_tfidf(query)
+    euclidean_results = []
+    cosine_results = []
 
+    queries = get_queries()
+    for i, query in enumerate(queries, 1):
+        binary_result = process_query_binary(query)
+        tf_result = process_query_term_frequency(query)
+        tfidf_result = process_query_tfidf(query)
+
+        relevant_docs = get_relevant_docs(i)
+
+        print("Got:      {}".format(binary_result))
+        print("Expected: {}".format(relevant_docs))
+
+        euclidean_results.append(Query_Results(
+            binary_precision=calculate_precision(binary_result.euclidean[:len(relevant_docs)], relevant_docs),
+            binary_recall=calculate_recall(binary_result.euclidean[:len(relevant_docs)], relevant_docs),
+            tf_precision=calculate_precision(tf_result.euclidean[:len(relevant_docs)], relevant_docs),
+            tf_recall=calculate_recall(tf_result.euclidean[:len(relevant_docs)], relevant_docs),
+            tfidf_precision=calculate_precision(tfidf_result.euclidean[:len(relevant_docs)], relevant_docs),
+            tfidf_recall=calculate_recall(tfidf_result.euclidean[:len(relevant_docs)], relevant_docs)
+        ))
+
+        cosine_results.append(Query_Results(
+            binary_precision=calculate_precision(binary_result.cosine[:len(relevant_docs)], relevant_docs),
+            binary_recall=calculate_recall(binary_result.cosine[:len(relevant_docs)], relevant_docs),
+            tf_precision=calculate_precision(tf_result.cosine[:len(relevant_docs)], relevant_docs),
+            tf_recall=calculate_recall(tf_result.cosine[:len(relevant_docs)], relevant_docs),
+            tfidf_precision=calculate_precision(tfidf_result.cosine[:len(relevant_docs)], relevant_docs),
+            tfidf_recall=calculate_recall(tfidf_result.cosine[:len(relevant_docs)], relevant_docs)
+        ))
+
+
+    binary_euclidean_precision_sum = 0
+    binary_cosine_precision_sum = 0
+    binary_euclidean_recall_sum = 0
+    binary_cosine_recall_sum = 0
+    tf_euclidean_precision_sum = 0
+    tf_cosine_precision_sum = 0
+    tf_euclidean_recall_sum = 0
+    tf_cosine_recall_sum = 0
+    tfidf_euclidean_precision_sum = 0
+    tfidf_cosine_precision_sum = 0
+    tfidf_euclidean_recall_sum = 0
+    tfidf_cosine_recall_sum = 0
+
+    for i, query in enumerate(queries, 1):
+        binary_euclidean_precision = euclidean_results[i - 1].binary_precision
+        binary_cosine_precision = cosine_results[i - 1].binary_precision
+        binary_euclidean_recall = euclidean_results[i - 1].binary_recall
+        binary_cosine_recall = cosine_results[i - 1].binary_recall
+        tf_euclidean_precision = euclidean_results[i - 1].tf_precision
+        tf_cosine_precision = cosine_results[i - 1].tf_precision
+        tf_euclidean_recall = euclidean_results[i - 1].tf_recall
+        tf_cosine_recall = cosine_results[i - 1].tf_recall
+        tfidf_euclidean_precision = euclidean_results[i - 1].tfidf_precision
+        tfidf_cosine_precision = cosine_results[i - 1].tfidf_precision
+        tfidf_euclidean_recall = euclidean_results[i - 1].tfidf_recall
+        tfidf_cosine_recall = cosine_results[i - 1].tfidf_recall
+
+        binary_euclidean_precision_sum +=binary_euclidean_precision
+        binary_cosine_precision_sum += binary_cosine_precision
+        binary_euclidean_recall_sum +=binary_euclidean_recall
+        binary_cosine_recall_sum +=binary_cosine_recall
+        tf_euclidean_precision_sum +=tf_euclidean_precision
+        tf_cosine_precision_sum += tf_cosine_precision
+        tf_euclidean_recall_sum +=tf_euclidean_recall
+        tf_cosine_recall_sum +=tf_cosine_recall
+        tfidf_euclidean_precision_sum +=tfidf_euclidean_precision
+        tfidf_cosine_precision_sum += tfidf_cosine_precision
+        tfidf_euclidean_recall_sum +=tfidf_euclidean_recall
+        tfidf_cosine_recall_sum +=tfidf_cosine_recall
+
+        print(
+            """
+            Query #{query_num}
+            -------------------------------------
+            Binary
+                - Precision:
+                    - Euclidean: {binary_euclidean_precision}
+                    - Cosine: {binary_cosine_precision}
+                - Recall:
+                    - Euclidean: {binary_euclidean_recall}
+                    - Cosine: {binary_cosine_recall}
+
+            Term frequency
+                - Precision:
+                    - Euclidean: {tf_euclidean_precision}
+                    - Cosine: {tf_cosine_precision}
+                - Recall:
+                    - Euclidean: {tf_euclidean_recall}
+                    - Cosine: {tf_cosine_recall}
+
+            TF-IDF
+                - Precision:
+                    - Euclidean: {tfidf_euclidean_precision}
+                    - Cosine: {tfidf_cosine_precision}
+                - Recall:
+                    - Euclidean: {tfidf_euclidean_recall}
+                    - Cosine: {tfidf_cosine_recall}
+            """.format(
+                query_num=i,
+                binary_euclidean_precision=binary_euclidean_precision,
+                binary_cosine_precision=binary_cosine_precision,
+                binary_euclidean_recall=binary_euclidean_recall,
+                binary_cosine_recall=binary_cosine_recall,
+                tf_euclidean_precision=tf_euclidean_precision,
+                tf_cosine_precision=tf_cosine_precision,
+                tf_euclidean_recall=tf_euclidean_recall,
+                tf_cosine_recall=tf_cosine_recall,
+                tfidf_euclidean_precision=tfidf_euclidean_precision,
+                tfidf_cosine_precision=tfidf_cosine_precision,
+                tfidf_euclidean_recall=tfidf_euclidean_recall,
+                tfidf_cosine_recall=tfidf_cosine_recall,
+
+            )
+        )
+
+
+    print(
+        """
+        -------------------------------------
+        AVERAGE
+        -------------------------------------
+        Binary
+            - Precision:
+                - Euclidean: {binary_euclidean_precision}
+                - Cosine: {binary_cosine_precision}
+            - Recall:
+                - Euclidean: {binary_euclidean_recall}
+                - Cosine: {binary_cosine_recall}
+
+        Term frequency
+            - Precision:
+                - Euclidean: {tf_euclidean_precision}
+                - Cosine: {tf_cosine_precision}
+            - Recall:
+                - Euclidean: {tf_euclidean_recall}
+                - Cosine: {tf_cosine_recall}
+
+        TF-IDF
+            - Precision:
+                - Euclidean: {tfidf_euclidean_precision}
+                - Cosine: {tfidf_cosine_precision}
+            - Recall:
+                - Euclidean: {tfidf_euclidean_recall}
+                - Cosine: {tfidf_cosine_recall}
+        """.format(
+            binary_euclidean_precision=binary_euclidean_precision_sum / len(queries),
+            binary_cosine_precision=binary_cosine_precision_sum / len(queries),
+            binary_euclidean_recall=binary_euclidean_recall_sum / len(queries),
+            binary_cosine_recall=binary_cosine_recall_sum / len(queries),
+            tf_euclidean_precision=tf_euclidean_precision_sum / len(queries),
+            tf_cosine_precision=tf_cosine_precision_sum / len(queries),
+            tf_euclidean_recall=tf_euclidean_recall_sum / len(queries),
+            tf_cosine_recall=tf_cosine_recall_sum / len(queries),
+            tfidf_euclidean_precision=tfidf_euclidean_precision_sum / len(queries),
+            tfidf_cosine_precision=tfidf_cosine_precision_sum / len(queries),
+            tfidf_euclidean_recall=tfidf_euclidean_recall_sum / len(queries),
+            tfidf_cosine_recall=tfidf_cosine_recall_sum / len(queries),
+
+        )
+    )
 
 process_queries()
