afefee2557dce85ad1809e238a587da30220b690
Martin Melka <melkamar@fit.cvut.cz>
2017-05-13 13:58:10 +0200

    HW6: Implement content-based recommending

--- homeworks/hw6/recommender.py
+++ homeworks/hw6/recommender.py
@@ -1,4 +1,7 @@
 import csv
+from typing import List, Dict, Tuple
+from pprint import pprint
+
 import numpy
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -6,93 +9,6 @@ numpy.set_printoptions(precision=2, linewidth=999)
 
 SKIP_GENRES = ['(no genres listed)']
 RATING_THRESHOLD = 2.5
-
-
-class Recommender:
-    def __init__(self, movies_csv, ratings_csv):
-        super().__init__()
-
-        self.movies_csv_fn = movies_csv
-        self.ratings_csv_fn = ratings_csv
-
-        self.movies, self.genres_list = self._read_movies()
-        self.genre_str_to_id = {genre: i for i, genre in enumerate(self.genres_list)}
-        self.genre_id_to_str = {i: genre for i, genre in enumerate(self.genres_list)}
-
-        self.users = self._read_users()
-
-        # pprint(genre_str_to_id)
-        for user in self.users.values():
-            user.print_genre_ratings(self.genre_id_to_str)
-
-    def recommend_content_based(self, user_id, top_n_results):
-        """
-        Calculate cosine similarity of the given User's ratings to all movies in the Recommender (similarity
-        of rated genres). Returns the top N genre-similar movies.
-        E.g. if user
-        :param user_id: ID of the user
-        :param top_n_results: Number of top results to return.
-        :return:
-        """
-        for movie in self.movies:
-            pass
-
-
-
-    def _read_movies(self):
-        """
-        Read datafile with movies.
-        :return: Tuple: (
-                         dict of Movie objects (key = movie_id),
-                         list of genres (as strings)
-                        )
-        """
-
-        genres_set = set()
-        movies = {}
-
-        with open(self.movies_csv_fn, encoding="utf-8") as f:
-            f.readline()
-            f.readline()
-            reader = csv.reader(f, delimiter=',')
-            for movie_row in reader:
-                # print("Row: {}".format(movie_row))
-                movie_id = movie_row[0]
-                title = movie_row[1]
-                genres = [genre.strip() for genre in movie_row[2].split('|') if genre.strip() not in SKIP_GENRES]
-                movie = Movie(movie_id, title, genres)
-                movies[movie_id] = movie
-                genres_set = genres_set.union(set(genres))
-
-        return movies, sorted(list(genres_set))
-
-    def _read_users(self):
-        users = {}
-
-        with open(self.ratings_csv_fn, encoding="utf-8") as f:
-            f.readline()
-            f.readline()
-            reader = csv.reader(f, delimiter=',', )
-            for i, rating_row in enumerate(reader):
-                user_id = rating_row[0]
-                movie_id = rating_row[1]
-                rating = float(rating_row[2])
-                if user_id not in users:
-                    users[user_id] = User(user_id, len(self.genre_str_to_id))
-
-                users[user_id].movies_rated.add(movie_id)
-                for movie_genre in self.movies[movie_id].genres:
-                    if rating >= RATING_THRESHOLD:
-                        users[user_id].ratings[self.genre_str_to_id[movie_genre]] += 1
-
-        # TODO Not sure about this - normalizing user rating vector to have scores in (0,1).
-        # Reason - Vector (3,1,0) would be closer to (0,1,0) than (3,0,0), but user clearly more prefers the first genre
-        for user in users.values():
-            print("{} Pre-normalization: {}".format(user.id, user.ratings))
-            user.ratings = user.ratings / numpy.amax(user.ratings)
-            print("{} Post-normalization: {}".format(user.id, user.ratings))
-
-        return users
 
 
 class User:
@@ -118,14 +34,123 @@ class Movie:
         self.id = movie_id
         self.title = title
         self.genres = genres
+        self.genres_vector = None
+
+    def populate_genres_vector(self, genres_str_to_int: Dict[str, int]):
+        self.genres_vector = numpy.zeros(len(genres_str_to_int))
+        for genre in self.genres:
+            genre_id = genres_str_to_int[genre]
+            self.genres_vector[genre_id] = 1
+
+        self.genres_vector = self.genres_vector.reshape(1, -1)
 
     def __repr__(self, *args, **kwargs):
         return "{} ({},{})".format(self.title, self.id, self.genres)
 
 
+class Recommender:
+    def __init__(self, movies_csv, ratings_csv):
+        super().__init__()
+
+        self.movies_csv_fn = movies_csv
+        self.ratings_csv_fn = ratings_csv
+
+        # self.movies: Dict[Movie], self.genres_list: List[str] = self._read_movies()
+        self.movies, self.genres_list = self._read_movies()
+        self.genre_str_to_id = {genre: i for i, genre in enumerate(self.genres_list)}
+        self.genre_id_to_str = {i: genre for i, genre in enumerate(self.genres_list)}
+        for movie in self.movies.values():
+            movie.populate_genres_vector(self.genre_str_to_id)
+
+        self.users: List[User] = self._read_users()
+
+    def print_user_ratings(self, user_id):
+        self.users[user_id].print_genre_ratings(self.genre_id_to_str)
+
+    def recommend_content_based(self, user_id: int, top_n_results: int) -> List[Tuple[int, float]]:
+        """
+        Calculate cosine similarity of the given User's ratings to all movies in the Recommender (similarity
+        of rated genres). Returns the top N genre-similar movies.
+        E.g. if user
+        :param user_id: ID of the user
+        :param top_n_results: Number of top results to return.
+        :return: List of (movie_id, similarity) tuples sorted in descending order based on similarity.
+        """
+        similarities: Dict[int, float] = {}
+
+        non_rated_movies = [movie for movie in self.movies.values() if movie.id not in self.users[user_id].movies_rated]
+        for movie in non_rated_movies:
+            similarity = cosine_similarity(self.users[user_id].ratings.reshape(1, -1), movie.genres_vector)[0][0]
+            similarities[movie.id] = similarity
+
+        # Sort obtained similarities, best first
+        sorted_similarities: List[Tuple[int, float]] = sorted(similarities.items(),
+                                                              key=lambda item: item[1],
+                                                              reverse=True)
+        return sorted_similarities[:top_n_results]
+
+    def _read_movies(self) -> Tuple[Dict[int, Movie], List[str]]:
+        """
+        Read datafile with movies.
+        :return: Tuple: (
+                         dict of Movie objects (key = movie_id),
+                         list of genres (as strings)
+                        )
+        """
+
+        genres_set = set()
+        movies = {}
+
+        with open(self.movies_csv_fn, encoding="utf-8") as f:
+            f.readline()
+            f.readline()
+            reader = csv.reader(f, delimiter=',')
+            for movie_row in reader:
+                # print("Row: {}".format(movie_row))
+                movie_id = int(movie_row[0])
+                title = movie_row[1]
+                genres = [genre.strip() for genre in movie_row[2].split('|') if genre.strip() not in SKIP_GENRES]
+                movie = Movie(movie_id, title, genres)
+                movies[movie_id] = movie
+                genres_set = genres_set.union(set(genres))
+
+        return movies, sorted(list(genres_set))
+
+    def _read_users(self):
+        users: Dict[User] = {}
+
+        with open(self.ratings_csv_fn, encoding="utf-8") as f:
+            f.readline()
+            f.readline()
+            reader = csv.reader(f, delimiter=',', )
+            for i, rating_row in enumerate(reader):
+                user_id = int(rating_row[0])
+                movie_id = int(rating_row[1])
+                rating = float(rating_row[2])
+                if user_id not in users:
+                    users[user_id] = User(user_id, len(self.genre_str_to_id))
+
+                users[user_id].movies_rated.add(movie_id)
+                for movie_genre in self.movies[movie_id].genres:
+                    if rating >= RATING_THRESHOLD:
+                        users[user_id].ratings[self.genre_str_to_id[movie_genre]] += 1
+
+        # TODO Not sure about this - normalizing user rating vector to have scores in (0,1).
+        # Reason - Vector (3,1,0) would be closer to (0,1,0) than (3,0,0), but user clearly more prefers the first genre
+        for user in users.values():
+            user.ratings = user.ratings / numpy.amax(user.ratings)
+
+        return users
+
+
 def main():
     recommender = Recommender('data/movies.csv', 'data/ratings.csv')
-    recommender.recommend_content_based(1, 5)
+    user_id = 1
+
+    recommender.print_user_ratings(user_id)
+    recommended_movies = recommender.recommend_content_based(user_id, 5)
+    pprint(recommended_movies)
+
 
 if __name__ == '__main__':
     main()
