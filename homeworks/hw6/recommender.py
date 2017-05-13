48ea6affc16fc45da853ac43d39a319a41e8e08b
Melka Martin <melka@avast.com>
2017-05-13 11:25:29 +0200

    HW6: Initialize users, initial data loading done

--- homeworks/hw6/recommender.py
+++ homeworks/hw6/recommender.py
@@ -1,13 +1,24 @@
+from pprint import pprint
 import csv
+import numpy
 
 SKIP_GENRES = ['(no genres listed)']
+RATING_THRESHOLD = 2.5
 
 
 class User:
-    def __init__(self, user_id, ratings):
+    def __init__(self, user_id, genres_count):
         super().__init__()
         self.id = user_id
-        self.ratings = ratings
+        self.ratings = numpy.zeros(genres_count)
+
+    # def __repr__(self, *args, **kwargs):
+    #     return "User[{}]: {}".format(self.id, self.ratings)
+
+    def print_genre_ratings(self, genre_id_to_str):
+        print("User {} ratings:".format(self.id))
+        for i in range(0, self.ratings.size):
+            print("  {}: {}".format(genre_id_to_str[i], self.ratings[i]))
 
 
 class Movie:
@@ -17,6 +28,9 @@ class Movie:
         self.title = title
         self.genres = genres
 
+    def __repr__(self, *args, **kwargs):
+        return "{} ({},{})".format(self.title, self.id, self.genres)
+
 
 def read_movies(fn):
     """
@@ -24,7 +38,7 @@ def read_movies(fn):
     :param fn:
     :return: Tuple: (
                      dict of Movie objects (key = movie_id),
-                     set of genres (as strings)
+                     list of genres (as strings)
                     )
     """
 
@@ -36,7 +50,7 @@ def read_movies(fn):
         f.readline()
         reader = csv.reader(f, delimiter=',')
         for movie_row in reader:
-            print("Row: {}".format(movie_row))
+            # print("Row: {}".format(movie_row))
             movie_id = movie_row[0]
             title = movie_row[1]
             genres = [genre.strip() for genre in movie_row[2].split('|') if genre.strip() not in SKIP_GENRES]
@@ -44,13 +58,50 @@ def read_movies(fn):
             movies[movie_id] = movie
             genres_set = genres_set.union(set(genres))
 
-    return movies, genres_set
+    return movies, sorted(list(genres_set))
+
+
+def read_users(fn, movies, genre_str_to_id):
+    users = {}
+
+    with open(fn, encoding="utf-8") as f:
+        f.readline()
+        f.readline()
+        reader = csv.reader(f, delimiter=',', )
+        for i, rating_row in enumerate(reader):
+            if i > 1:
+                break
+            print("Row: {}".format(rating_row))
+            user_id = rating_row[0]
+            movie_id = rating_row[1]
+            rating = float(rating_row[2])
+            if user_id not in users:
+                users[user_id] = User(user_id, len(genre_str_to_id))
+
+            for movie_genre in movies[movie_id].genres:
+                if rating >= RATING_THRESHOLD:
+                    users[user_id].ratings[genre_str_to_id[movie_genre]] += 1
+
+                    # genres = [genre.strip() for genre in rating_row[2].split('|') if genre.strip() not in SKIP_GENRES]
+                    # movie = Movie(movie_id, title, genres)
+                    # users[movie_id] = movie
+                    # genres_set = genres_set.union(set(genres))
+
+    return users
 
 
 def main():
     movies, genres = read_movies('data/movies.csv')
+    genre_str_to_id = {genre: i for i, genre in enumerate(genres)}
+    genre_id_to_str = {i: genre for i, genre in enumerate(genres)}
 
-    print("Genres: {}".format(genres))
+    users = read_users('data/ratings.csv', movies, genre_str_to_id)
+
+    pprint(genre_str_to_id)
+    for user in users.values():
+        user.print_genre_ratings(genre_id_to_str)
+    # print("Genres: {}".format(genres))
+    # print(movies)
 
 
 if __name__ == '__main__':
